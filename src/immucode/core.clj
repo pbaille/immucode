b5bd831e8625dd877a677a74cbd31adea63e5209
pbaille <pierrebaille@gmail.com>
2023-01-21 09:28:55 +0100

    let and lambda, almost

--- src/immucode/core.clj
+++ src/immucode/core.clj
@@ -77,8 +77,6 @@
 
 (do :two
 
-    (def ENV0 {})
-
     (do :help
 
         (defn append1 [l x]
@@ -97,48 +95,145 @@
         (defn cd [env sym]
           (tree/cd env (path/path sym)))
 
+        (defn bubfind [env x]
+          (if (symbol? x)
+            (tree/find env (path/path x))))
+
         (defn env-get [env at]
           (tree/cd (tree/root env) (path/path at)))
 
         (defn expression-subenvs [env]
           (assert (:expression env))
           (map (fn [idx] (tree/cd env [idx]))
-               (range (count (:expression env)))))
+               (range (count (:expression env))))))
 
-        (defn compile1
-          [{:as env :keys [link expression value var]}]
-          (cond
-            var (var->qualified-symbol var)
-            value value
-            link (path->var-sym link)
-            expression (map compile1 (expression-subenvs env)))))
+    (defn evaluate
+
+      ([{:as env :keys [expression link var value]}]
+       (or value
+           (if-let [f (:evaluate env)] (f env))
+           (cond
+             var (deref var)
+             link (evaluate (tree/at env link))
+             expression (let [[verb & args]
+                              (map evaluate
+                                   (expression-subenvs env))]
+                          (apply verb args)))))
+
+      ([env expr]
+
+       (cp expr
+
+           symbol? (if-let [found (bubfind env expr)]
+                     (evaluate found)
+                     (if-let [resolved (resolve expr)]
+                       (deref resolved)
+                       (u/throw [:unresolvable expr :in env])))
+
+           seq? (let [verb (bubfind env (first expr))]
+                  (if-let [f (:evaluate verb)]
+                    (f env (rest expr))
+                    (let [[verb & args]
+                          (map (partial evaluate env) expr)]
+                      (apply verb args))))
+
+           expr))
+
+      ([env at expr]
+       (evaluate (cd env at) expr)))
 
     (defn bind
 
       ([env expr]
+
        (cp expr
-           symbol? (if-let [found (tree/find env (path/path expr))]
-                     (assoc env :link (tree/position found))
+
+           symbol? (if-let [found (bubfind env expr)]
+                     (if (:local found)
+                       (assoc env :local (:local found))
+                       (assoc env :link (tree/position found)))
                      (if-let [resolved (resolve expr)]
                        (assoc env :var resolved)
                        (u/throw [:unresolvable expr :in env])))
 
-           seq? (reduce  (fn [env [idx subexpr]]
-                           (bind env idx subexpr))
-                         (assoc env :expression expr)
-                         (map-indexed vector expr))
+           seq? (let [verb (bubfind env (first expr))]
+                  (if-let [f (:bind verb)]
+                    (f env (rest expr))
+                    (reduce (fn [env [idx subexpr]]
+                              (bind env idx subexpr))
+                            (assoc env :expression expr)
+                            (map-indexed vector expr))))
 
            (assoc env :value expr)))
 
       ([env sym expr]
        (let [path (path/path sym)]
          (if (tree/cd env path)
-           (u/throw "already defined path " sym env)
+           (u/throw [::bind "already defined path" sym env])
            (tree/upd (tree/ensure-path env path)
                      path
                      #(bind % expr))))))
 
+    (def ENV0
+
+      (-> {}
+          ;; lambda
+          (tree/put '[fn] {:evaluate
+                          (fn [env [argv return]]
+                            (fn [& xs]
+                              (let [env+ (reduce (fn [e [argsym argval]]
+                                                   (tree/put e [argsym] :value argval))
+                                                 env (zipmap argv (map (partial evaluate env) xs)))]
+                                (evaluate env+ return))))
+
+                          :bind
+                          (fn [env [argv return]]
+                            (let [retsym (gensym "ret_")
+                                  return-path (conj (tree/position env) retsym)
+                                  form (list 'fn argv return)]
+                              (bind (reduce (fn [e a]
+                                              (tree/put e [a] :local a))
+                                            (assoc env
+                                                   :lambda form
+                                                   :argv argv
+                                                   :return return-path)
+                                            argv)
+                                    retsym
+                                    return)))})
+          ;; let
+          (tree/put '[let] {:evaluate
+                            (fn [env [bindings return]]
+                              (let [env+ (reduce (fn [e [argsym argval]]
+                                                   (tree/put e [argsym] :value (evaluate e argval)))
+                                                 env (partition 2 bindings))]
+                                (evaluate env+ return)))
+
+                            :bind
+                            (fn [env [bindings return]]
+                              (bind (reduce (fn [e [sym expr]]
+                                              (bind e sym expr))
+                                            (assoc env :form (list 'let bindings return))
+                                            (partition 2 bindings))
+                                    return))})))
+
     (do :bind
+        (-> ENV0
+            (bind 'fun '(fn [a] a))
+            (bind 'ret '(fun 1 2))
+            (cd 'ret)
+            (compile))
+
+        (-> ENV0
+            (bind 'ret '(let [a 1 b 2] (+ a b)))
+            (cd 'ret)
+            (compile))
+
+        (-> ENV0
+            (bind 'f '(fn [x] (let [a 1 b 2] (+ x a b))))
+            (bind 'ret '(f 1))
+            (cd 'ret)
+            (compile))
+
         (def E1
           (-> ENV0
               (bind 'x 1)
@@ -146,20 +241,9 @@
               (bind 'y 2)
               (bind 'a 34)
               (bind 'z '(+ x y))
-              (bind 'ret '(+ z z)))))
-
-    (defn evaluate
-      ([{:as env :keys [link expression value var]}]
-       (cond
-         var (deref var)
-         value value
-         link (evaluate (tree/at env link))
-         expression (let [[verb & args]
-                          (map evaluate
-                               (expression-subenvs env))]
-                      (apply verb args))))
-      ([env at]
-       (evaluate (cd env at))))
+              (bind 'ret '(+ z z))
+              (bind 'fun '(fn [a b c] (+ a b c z)))
+              (bind 'ret2 '(fun x ret a)))))
 
     (do :evaluate
         E1
@@ -167,30 +251,70 @@
         (evaluate E1 'x)
         (evaluate E1 'x-link)
         (evaluate E1 'z)
-        (evaluate E1 'ret))
+        (evaluate E1 'ret)
+        ((evaluate ENV0 '(fn [a] a))
+         1))
 
     (defn deps
-      [{:as env :keys [link expression]}]
+      [{:as env :keys [lambda link expression]}]
       (cond
         link (append1 (deps (tree/at env link)) link)
+        lambda (deps (tree/at env (:return env)))
         expression (reduce
                     (fn [ds subenv]
-                      (concat (remove (set ds) (deps subenv)) ds))
+                      (let [ds+ (deps subenv)]
+                        (concat ds+ (remove (set ds+) ds))))
                     () (expression-subenvs env))))
 
     (do :deps
-        (deps (cd E1 'ret)))
+        (deps (cd E1 'ret))
+        (deps (cd E1 'ret2)))
 
-    (defn compile [env at]
-      (let [bindings (->> (deps (cd env at))
-                          (map (partial tree/at env))
-                          (map (fn [env] (list 'def (path->var-sym (tree/position env)) (compile1 env))))
-                          (cons 'do))]
-        (append1 bindings
-                 (compile1 (cd env at)))))
+    (comment :useless?
+
+             (defn remove-duplicates [xs]
+               (loop [seen #{} todo xs ret []]
+                 (if-let [[x & xs] (seq todo)]
+                   (if (seen x)
+                     (recur seen xs ret)
+                     (recur (conj seen x) xs (conj ret x)))
+                   (seq ret))))
+
+             (defn undup-stack-conj [q x]
+               (cons x (remove (partial = x) q)))
+
+             (defn exploration
+               [seen {:as env :keys [local value link expression]}]
+               (cond local seen
+                     value seen
+                     link (exploration (undup-stack-conj seen link) (tree/at env link))
+                     expression (expression-subenvs env))))
+
+    (defn compile1
+      [{:as env :keys [lambda local link expression value var]}]
+      (or value
+          local
+          (cond
+            var (var->qualified-symbol var)
+            link (path->var-sym link)
+            lambda `(fn ~(:argv env) ~(compile1 (tree/at env (:return env))))
+            expression (map compile1 (expression-subenvs env)))))
+
+    (defn compile
+      ([env {:keys [bind-return-compiler]}]
+       (bind-return-compiler
+        (->> (deps env)
+             (map (partial tree/at env))
+             (map (juxt env->var-sym compile1)))
+        (compile1 env)))
+      ([env]
+       (compile env {:bind-return-compiler
+                     (fn [bindings return]
+                       `(let ~(vec (mapcat identity bindings)) ~return))})))
 
     (do :compile
-        (compile E1 'ret))
+        (compile (cd E1 'ret))
+        (eval (compile (cd E1 'ret2))))
 
 
 
